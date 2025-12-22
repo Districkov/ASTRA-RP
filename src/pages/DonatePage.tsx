f8544de04425d344f0b868933fe34abb7631df36
Districk <149391083+Districkov@users.noreply.github.com>
2025-12-22 18:57:54 +0300

    12

--- src/pages/DonatePage.tsx
+++ src/pages/DonatePage.tsx
@@ -315,7 +315,7 @@ function DonatePage() {
                 <label className="checkbox-label">
                   <input
                     type="checkbox"
-                    checked={agreeTerms}
+                    
                     onChange={(e) => setAgreeTerms(e.target.checked)}
                     required
                   />
